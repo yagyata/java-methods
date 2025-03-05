7fd72a86972c274784d80c2d703468cca60a8646
Yagyata <yagyatasharmaa@gmail.com>
2025-03-05 20:11:18 +0530

    [Yagyata] ADD: Performing actions on a number

--- NumberChecker3.java
+++ NumberChecker3.java
@@ -1,59 +1,85 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class NumberChecker3 {
 
+    // Method to find the count of digits in the number
     public static int countDigits(int number) {
-        return String.valueOf(number).length();
+        int count = 0;
+        while (number > 0) {
+            count++;
+            number /= 10;
+        }
+        return count;
     }
 
+    // Method to store digits in an array
     public static int[] getDigitsArray(int number) {
-        String numStr = String.valueOf(number);
-        int[] digits = new int[numStr.length()];
-        for (int i = 0; i < numStr.length(); i++) {
-            digits[i] = Character.getNumericValue(numStr.charAt(i));
+        int numDigits = countDigits(number);
+        int[] digits = new int[numDigits];
+
+        for (int i = numDigits - 1; i >= 0; i--) {
+            digits[i] = number % 10;
+            number /= 10;
         }
         return digits;
     }
 
-    public static int[] reverseArray(int[] array) {
-        int[] reversed = Arrays.copyOf(array, array.length);
-        for (int i = 0, j = reversed.length - 1; i < j; i++, j--) {
-            int temp = reversed[i];
-            reversed[i] = reversed[j];
-            reversed[j] = temp;
+    // Method to reverse the digits array
+    public static int[] reverseArray(int[] digits) {
+        int[] reversed = new int[digits.length];
+        for (int i = 0; i < digits.length; i++) {
+            reversed[i] = digits[digits.length - 1 - i];
         }
         return reversed;
     }
 
+    // Method to compare two arrays
     public static boolean areArraysEqual(int[] arr1, int[] arr2) {
-        return Arrays.equals(arr1, arr2);
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 
+    // Method to check if a number is a palindrome
     public static boolean isPalindrome(int number) {
         int[] digits = getDigitsArray(number);
-        int[] reversedDigits = reverseArray(digits);
-        return areArraysEqual(digits, reversedDigits);
+        int[] reversed = reverseArray(digits);
+        return areArraysEqual(digits, reversed);
     }
 
+    // Method to check if a number is a Duck Number
     public static boolean isDuckNumber(int number) {
-        String numStr = String.valueOf(number);
-        return numStr.contains("0") && numStr.charAt(0) != '0';
+        while (number > 0) {
+            if (number % 10 == 0) {
+                return true;
+            }
+            number /= 10;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
-        int number = 12321;
-        int duckNumber = 1023;
-
-        System.out.println("Count of digits: " + countDigits(number));
-
-        int[] digitsArray = getDigitsArray(number);
-        System.out.println("Digits Array: " + Arrays.toString(digitsArray));
-
-        int[] reversedArray = reverseArray(digitsArray);
-        System.out.println("Reversed Array: " + Arrays.toString(reversedArray));
-
+        Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number: ");
+        int number = sc.nextInt();
+        
+        System.out.println("Number: " + number);
+        System.out.println("Digit Count: " + countDigits(number));
+        
+        int[] digits = getDigitsArray(number);
+        System.out.println("Digits: " + Arrays.toString(digits));
+        
+        int[] reversedDigits = reverseArray(digits);
+        System.out.println("Reversed Digits: " + Arrays.toString(reversedDigits));
+        
         System.out.println("Is Palindrome: " + isPalindrome(number));
-
-        System.out.println("Is Duck Number: " + isDuckNumber(duckNumber));
+        System.out.println("Is Duck Number: " + isDuckNumber(number));
     }
 }
