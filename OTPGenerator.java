10b78f295a92a8b742133e76385303501e889657
Yagyata <yagyatasharmaa@gmail.com>
2025-03-05 20:42:23 +0530

    [Yagyata] ADD: Displaying calender for given month and year

---
+++ OTPGenerator.java
@@ -0,0 +1,36 @@
+public class OTPGenerator {
+
+    // Method to generate a 6-digit OTP using Math.random()
+    public static int generateOTP() {
+        return 100000 + (int) (Math.random() * 900000);
+    }
+
+    // Method to check if generated OTPs are unique
+    public static boolean areOTPsUnique(int[] otps) {
+        for (int i = 0; i < otps.length; i++) {
+            for (int j = i + 1; j < otps.length; j++) {
+                if (otps[i] == otps[j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] otps = new int[10];
+
+        // Generate 10 OTPs
+        for (int i = 0; i < 10; i++) {
+            otps[i] = generateOTP();
+            System.out.println("Generated OTP " + (i + 1) + ": " + otps[i]);
+        }
+
+        // Check uniqueness
+        if (areOTPsUnique(otps)) {
+            System.out.println("All OTPs are unique.");
+        } else {
+            System.out.println("Duplicate OTPs detected!");
+        }
+    }
+}
